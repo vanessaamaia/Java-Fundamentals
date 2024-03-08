5968042b8f3e6e7b6c3b8fd1c65dda5c0b4e8d77
VanessaMaia26 <vanessamaia2607@gmail.com>
2024-03-08 13:03:39 -0300

    polymorphism

--- pessoa/Professor.java
+++ pessoa/Professor.java
@@ -11,4 +11,9 @@ public class Professor extends Pessoa {
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
+    String imprimirDadosDaPessoa() {
+        System.out.println(super.imprimirDadosDaPessoa());
+        return "Você é professor";
+    }
 }
---
+++ veiculo/Moto.java
@@ -0,0 +1,14 @@
+package veiculo;
+
+public class Moto implements Veiculo {
+
+    @Override
+    public void acelerar() {
+        System.out.println("Acelerando a moto");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("Freando a moto");
+    }
+}
